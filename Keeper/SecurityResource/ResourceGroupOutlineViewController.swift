8341abae068756bfa9005b5520a2eb34114a25ae
panqingrong <panpanqingrong@icloud.com>
2017-12-13 11:39:39 +0800

    Add a edit window from adding and editing resource group

--- Keeper/SecurityResource/ResourceGroupOutlineViewController.swift
+++ Keeper/SecurityResource/ResourceGroupOutlineViewController.swift
@@ -8,18 +8,52 @@
 
 import Cocoa
 
+let RESOURCE_GROUP_EDIT_WINDOW_CONTROLLER = NSStoryboard.SceneIdentifier.init("ResourceGroupEditWindowController")
+
+class ResourceGroupValueObject{
+    var name:String!
+    var available:Bool = true
+}
+
 class ResourceGroupOutlineViewController:NSViewController{
     @IBOutlet var outlineView:NSOutlineView!
     @IBOutlet var treeController:NSTreeController!
     var managedObjectContext:NSManagedObjectContext!
+    var editWindowController:NSWindowController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        editWindowController = storyboard?.instantiateController(withIdentifier: RESOURCE_GROUP_EDIT_WINDOW_CONTROLLER) as! NSWindowController
+        
+    }
     
     override func viewDidAppear() {
         super.viewDidAppear()
         self.managedObjectContext = self.view.window!.windowController!.document!.managedObjectContext
     }
     
-    @IBAction func addResourceGroup(sender:NSButton){
+    func addResouceGroup(valueObject:ResourceGroupValueObject){
+        let newResourceGroup = NSEntityDescription.insertNewObject(forEntityName: "ResourceGroup", into: self.managedObjectContext)
+        newResourceGroup.setValue(valueObject.name, forKey: "name")
+        newResourceGroup.setValue(valueObject.available, forKey: "available")
         
+        
+    }
+    
+    
+    @IBAction func addResourceGroup(sender:NSButton){
+        let editViewController = editWindowController.contentViewController as! ResourceGroupEditViewController
+        view.window!.beginSheet(editWindowController.window!) { (modelResponse) in
+            if modelResponse == NSApplication.ModalResponse.OK{
+                let valueObject = ResourceGroupValueObject()
+                valueObject.name = editViewController.savedValues[RESOURCE_GROUP_SAVED_VALUES_NAME] as! String
+                if let isAvailable = editViewController.savedValues[RESOURCE_GROUP_SAVED_VALUES_IS_VAILABLE] as? Bool{
+                    valueObject.available = isAvailable
+                }
+                
+                self.addResouceGroup(valueObject: valueObject)
+            }
+        }
     }
     
     @IBAction func removeResourceGroup(sender:NSButton){
